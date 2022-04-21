6157356730f721a3d220ff125b5a7d724e8fad9d
Alexandr Sinkov <sinkov86@yandex.ru>
2022-04-21 23:31:01 +0300

    7th ready

--- 7.py
+++ 7.py
@@ -14,13 +14,17 @@ xList = [0,0,0,0,1,1,1,1]
 yList = [0,0,1,1,0,0,1,1]
 zList = [0,1,0,1,0,1,0,1]
 
-for i in range(0,7):
-    X = xList(i)
-    Y = yList(i)
-    Z = zList(i)
+for i in range(0,8):
+    X = xList[i]
+    Y = yList[i]
+    Z = zList[i]
 
 
-    if not(X or Y or Z) == (not X) and (not Y) and (not Z):
+    if not(X or Y or Z) == ((not X) and (not Y) and (not Z)):
         print(f'Равенство истинно при значениях ¬({X} ⋁ {Y} ⋁ {Z}) = ¬{X} ⋀ ¬{Y} ⋀ ¬{Z}')
+        # print(f'Левая часть {not(X or Y or Z)}')
+        # print(f'Правая часть {(not X) and (not Y) and (not Z)}')
     else:
         print(f'Равенство не истинно: ¬({X} ⋁ {Y} ⋁ {Z}) = ¬{X} ⋀ ¬{Y} ⋀ ¬{Z}')
+        # print(f'Левая часть {not(X or Y or Z)}')
+        # print(f'Правая часть {(not X) and (not Y) and (not Z)}')
