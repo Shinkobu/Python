79eb80302609cb311fb13ae90a6fba18960967da
Alexandr Sinkov <sinkov86@yandex.ru>
2022-05-03 10:10:05 +0300

    15th done

--- 15.py
+++ 15.py
@@ -2,4 +2,15 @@
 # Пример: пусть N = 4, тогда
 # [ 1, 2, 6, 24 ]
 
+n = int(input("Введите N \n"))
+i=1
+r=1
+resultList = []
+
+for i in range(1,n+1):
+    r=r*i
+    resultList.append (r)
+
+print(f"Набор произведений от 1 до {n}: \n {resultList}")
+
 
