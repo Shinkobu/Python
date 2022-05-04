63f88e72e9d46f53730ec69d13fff86c08d2dc17
Alexandr Sinkov <sinkov86@yandex.ru>
2022-05-04 19:56:26 +0300

    17th done

--- 17.py
+++ 17.py
@@ -14,10 +14,22 @@ print(f"Последовательность: \n {resultList}")
 #  считываем позиции из файла и находим результат 
 
 # Берём данные из 1 файла
+indexList = []
 data = open('file.txt', 'r')
+
 for line in data:
-    S = int(data.readline())
-    print(S)
+    S = int(line)
+    indexList.append (S)
 data.close
 
+print (f"Список с номерами элементов: \n {indexList}")
+
+# Вычисляем произведение элементов
+productResult = 1
+
+for j in range(0,len(indexList)):
+    productResult *= resultList[indexList[j]-1]
+    # print (productResult)
+
+print (f"Произведение элементов: \n {productResult}")
 
