40ea8f80c0c94909d633aa9a1416268932587daa
Alexandr Sinkov <sinkov86@yandex.ru>
2022-05-04 23:20:47 +0300

    18th done

--- 18.py
+++ 18.py
@@ -1,2 +1,17 @@
 # 18. Реализовать алгоритм перемешивания списка. 
 
+import random
+
+
+newList = [1,2,3,4,5,6,7,8,9,10]
+
+print(f"Исходный список: \n {newList}")
+
+for i in range(0,len(newList)-1):
+    j = random.randint(0,len(newList)-1)
+    tempElement = newList[i]
+    newList[i] = newList[j]
+    newList[j] = tempElement
+
+print(f"\n Перемешаный список: \n {newList}")
+    
