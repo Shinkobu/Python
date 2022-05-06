db6c042c0683fd1f8e73d62b025321daec7d90d2
Alexandr Sinkov <sinkov86@yandex.ru>
2022-05-06 19:24:35 +0300

    19th done

--- 19.py
+++ 19.py
@@ -1,1 +1,21 @@
 # 19 Реализовать алгоритм задания случайных чисел. Без использования встроенного генератора псевдослучайных чисел
+
+
+# Будем генерить случайное число от 0 до 9 от системного времени
+
+from datetime import datetime
+
+sumSec = 0
+
+for i in range(0,100000):
+    currentMicrosecond = datetime.now().microsecond
+    # print(currentMicrosecond)
+    sumSec += currentMicrosecond 
+
+# print(sumSec)
+
+rand = sumSec % 10
+print(f"Случайное число: {rand}")
+
+
+
