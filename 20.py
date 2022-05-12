a9799ce7dfd1a835417ca384311186fa8f2bdf92
Alexandr Sinkov <sinkov86@yandex.ru>
2022-05-12 20:11:38 +0300

    20th in progress

--- 20.py
+++ 20.py
@@ -1,3 +1,20 @@
 # 20. Определить, присутствует ли в заданном списке строк, некоторое число 
 
+# сделать список строк
 
+stringList = ["qwe", "asd2", "zxc1", "qwe6", "ertqwe"]
+N = str(input(f"Введите число для поиска \n"))
+
+
+# алгоритм поиска числа в списке
+
+def findNumber(N,stringList):
+    F=0
+    for i in range(0,len(stringList)):
+        F = stringList[i].find(N)
+        print(F)    
+
+             
+
+
+findNumber(N,stringList)
