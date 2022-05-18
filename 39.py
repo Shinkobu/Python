2b02219b2c8f00a9f77a2aacb47eaa8933f0377f
Alexandr Sinkov <sinkov86@yandex.ru>
2022-05-18 21:57:57 +0300

    39th added dumBot algo

--- 39.py
+++ 39.py
@@ -34,7 +34,41 @@ def candyGame (Total,pList):
         
     print (f"На столе осталось {Total} конфет! Последние конфеты взял {pList[turnNumber%2]}.\n Победил {pList[((turnNumber)%2)]} !!!")
 
+   
+# Игра против примитивного бота
+
+def candyGameBot (Total,pList):
+
+    pList[1] = "Компьютер"
+    # Жеребьёвка
+    turnNumber = random.randint(-1,0)
+    print (f"\n По результатам жеребьёвки первым ходит {pList[turnNumber+1]}\n")
+
+    # Игра
+    while Total > 0:
+
+        turnNumber += 1
+
+        # Блок выбора бота с проверкой значения
+        if pList[turnNumber%2] == "Компьютер":
+            print(f"Ходит {pList[turnNumber%2]}.\n На столе {Total} конфет. Сколько конфет берёшь?")
+            turn = random.randint(1,28)
+            while turn > Total:
+                turn = random.randint(1,28)
+            print(turn)
+        else:
+            turn = int(input(f"Ходит {pList[turnNumber%2]}.\n На столе {Total} конфет. Сколько конфет берёшь? \n"))
+                   
+        # Проверка значения для человека
+            while turn > 28 or turn > Total:
+                turn = int(input(f"За один ход можно забрать не более чем 28 конфет и не больше, чем лежит на столе!  \n"))
+        
+        Total = Total - turn
+        
+    print (f"На столе осталось {Total} конфет! Последние конфеты взял {pList[turnNumber%2]}.\n Победил {pList[((turnNumber)%2)]} !!!")
+
+
 Total = 50
 pList = ["Игрок 1","Игрок 2"]
-    
-candyGame (Total,pList)
+
+candyGameBot (Total,pList)
