ee33536327910c23e5e81aa70aa271c415e35f27
Alexandr Sinkov <sinkov86@yandex.ru>
2022-05-22 22:07:43 +0300

    17th upgraded

--- 17.py
+++ 17.py
@@ -1,35 +1,62 @@
 # 17 Задать список из N элементов, заполненных числами из [-N, N]. Найти произведение элементов на указанных позициях. Позиции хранятся в файле file.txt в одной строке одно число
 
-n = int(input("Введите N \n"))
+from itertools import product
+from math import prod
 
-# создаём список [-N, N]
-i=1
-resultList = []
 
-for i in range(-n,n+1):
-    resultList.append (i)
-   
-print(f"Последовательность: \n {resultList}")
+def task17_1 ():
+    n = int(input("Введите N \n"))
 
-#  считываем позиции из файла и находим результат 
+    # создаём список [-N, N]
+    i=1
+    resultList = []
 
-# Берём данные из 1 файла
-indexList = []
-data = open('file.txt', 'r')
+    for i in range(-n,n+1):
+        resultList.append (i)
+    
+    print(f"Последовательность: \n {resultList}")
 
-for line in data:
-    S = int(line)
-    indexList.append (S)
-data.close
+    #  считываем позиции из файла и находим результат 
 
-print (f"Список с номерами элементов: \n {indexList}")
+    # Берём данные из 1 файла
+    indexList = []
+    data = open('file.txt', 'r')
 
-# Вычисляем произведение элементов
-productResult = 1
+    for line in data:
+        S = int(line)
+        indexList.append (S)
+    data.close
 
-for j in range(0,len(indexList)):
-    productResult *= resultList[indexList[j]-1]
-    # print (productResult)
+    print (f"Список с номерами элементов: \n {indexList}")
 
-print (f"Произведение элементов: \n {productResult}")
+    # Вычисляем произведение элементов
+    productResult = 1
 
+    for j in range(0,len(indexList)):
+        productResult *= resultList[indexList[j]-1]
+        # print (productResult)
+
+    print (f"Произведение элементов: \n {productResult}")
+
+def task17_2 ():
+    n = int(input("Введите N \n"))
+
+    # создаём список [-N, N]
+    resultList = [i for i in range(-n,n+1)]
+    print(f"Последовательность: \n {resultList}")
+
+    #  считываем позиции из файла и находим результат 
+    # Берём данные из 1 файла
+
+    data = open('file.txt', 'r')
+    indexList = [int(line) for line in data]
+    data.close
+
+    print (f"Список с номерами элементов: \n {indexList}")
+
+    # Вычисляем произведение элементов
+    
+    productResult = prod([resultList[indexList[i]-1] for i in range(len(indexList))])
+    print (f"Произведение элементов: \n {productResult}")
+
+task17_2()
